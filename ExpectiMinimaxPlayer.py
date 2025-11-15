eae8c1c8bc14d244f4bd11a6c1ec56885c14b289
JusBueno <justinbueno68@gmail.com>
2025-11-15 16:59:17 -0600

    Updated initialization of Expectiminimax

--- ExpectiMinimaxPlayer.py
+++ ExpectiMinimaxPlayer.py
@@ -1,6 +1,5 @@
 from Player import Player
 
 class ExpectiMinimaxPlayer(Player): 
-
-    def __init__(self, hand): 
-        super().__init__(hand)
+    def __init__(self, name : str = "ExpectiMinimax"): 
+        super().__init__(name)
