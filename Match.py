7515bef2db01cce055338a7c978743586fa47d9c
JusBueno <justinbueno68@gmail.com>
2025-11-15 13:57:16 -0600

    added priority ordering for who goes first

--- Match.py
+++ Match.py
@@ -64,12 +64,11 @@ class Match():
         # Conversation is allowed to find out who is first
 
         # Order of domino tiles (which tile goes first)
-        priority_order : list[Domino] = [] 
+        priority_order : list[Domino] = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (3, 3), (3, 4), (3, 5), (3, 6), (4, 4), (4, 5), (4, 6), (5, 5), (5, 6), (6, 6)]
         
-        # Adding tiles to priority order
-        # Doubles
-        for i in range(6,0,-1):
-            priority_order.append((i,i))
+        # Ordering list according to priority rules
+        priority_order.sort(key = lambda tile: (tile[0] + tile[-1]) * (100 if tile[0] == tile[-1] else 1))
+
 
 
 
